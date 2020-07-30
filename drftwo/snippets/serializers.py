c4a9a7d56376de1b14337a7d869021f0ccec08a5
gbelecedric <cedric.gbele@uvci.edu.ci>
2020-07-30 14:38:56 +0000

    s

--- drftwo/snippets/serializers.py
+++ drftwo/snippets/serializers.py
@@ -31,4 +31,4 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
-        fields = "_all_"
+        fields = "__all__"
