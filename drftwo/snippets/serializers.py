dc7b72a6bdec4a2f42aa9d296b05849e84d386a9
gbelecedric <cedric.gbele@uvci.edu.ci>
2020-07-30 21:46:08 +0000

    d

--- drftwo/snippets/serializers.py
+++ drftwo/snippets/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
-
+from django.contrib.auth.models import User
 
 # class SnippetSerializer(serializers.Serializer):
 #     id = serializers.IntegerField(read_only=True)
@@ -28,6 +28,13 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 #         instance.save()
 #         return instance
 
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
